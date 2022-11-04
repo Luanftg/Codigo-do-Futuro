bafa92d3373c2907a36fc08d86bbc163610121d5
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-11-04 20:38:11 -0300

    [Aula 01]: utilizando print e alert no console do navegador

---
+++ aula_prompt_alert.js
@@ -0,0 +1,7 @@
+var name = prompt("Informe o nome do cliente");
+var alvejante = 3.6;
+var sabaoEmPo = 5.6;
+var sabonete = 1.5;
+var result = alvejante + sabaoEmPo + sabonete;
+
+alert(`Ola ${name}, o valor da sua compra é de ${result}`);
