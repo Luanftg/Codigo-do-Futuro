f14be3f28a93e3e6a4259a59b950b9e504b50846
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-11-05 13:05:27 -0300

    [Aula 02]: adiciona exercício aula 02

---
+++ Aula02-JavaScript-Prática/exerc02Aula02.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var nomeDoAluno = prompt('Informe o nome do aluno');
+var somaDasNotas = 0;
+for(i=0;i<=3;i++) {
+    
+    var nota = Number(prompt('Informe a primeira nota do aluno'));
+    somaDasNotas = somaDasNotas + nota;
+}
+var media = somaDasNotas /4;
+if(media >= 5 ) {
+    console.log(`Aluno ${nomeDoAluno} aprovado`);
+} else {
+    console.log(`Aluno ${nomeDoAluno} reprovado`);
+}
+console.log(media); 
+// var nota2 = rl.input('Informe a primeira nota do aluno');
+// var nota3 = rl.input('Informe a primeira nota do aluno');
+// var nota4 = rl.input('Informe a primeira nota do aluno');
+
+
