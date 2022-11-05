b240ae6f73baa410855ddc3b353e852eded751a7
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-11-04 22:03:23 -0300

    [refatoração]: arredondamento de 2 casas decimais

--- aula01_exercicio2_js.js
+++ aula01_exercicio2_js.js
@@ -5,4 +5,4 @@ var sabonete = parseFloat(prompt("Informe o preço do Sabonete"));
 
 var result = alvejante + sabaoEmPo + sabonete;
 
-alert(`Ola ${name}, o valor da sua compra é de ${alvejante} + ${sabaoEmPo} + ${sabonete} =  ${result}`);
+alert(`Ola ${name}, o valor da sua compra é de ${alvejante} + ${sabaoEmPo} + ${sabonete} =  ${result.toFixed(2)}`);
